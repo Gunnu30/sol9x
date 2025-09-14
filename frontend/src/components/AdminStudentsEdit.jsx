0e1ace3b2ce0a4333bcef65ff3abdbe870dc4cc0
PUSHPAK ASHRITH <147341489+Gunnu30@users.noreply.github.com>
2025-09-14 13:29:45 +0530

    Update AdminStudentsEdit.jsx

--- frontend/src/components/AdminStudentsEdit.jsx
+++ frontend/src/components/AdminStudentsEdit.jsx
@@ -22,7 +22,7 @@ const EditStudent = () => {
   useEffect(() => {
     const fetchStudent = async () => {
       try {
-        const res = await fetch(`https://so3h-backend.vercel.app/api/admin/student/${id}`);
+        const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/admin/student/${id}`);
         const data = await res.json();
         setStudent(data.student);
         setFormData({
@@ -62,7 +62,7 @@ const EditStudent = () => {
       };
 
    
-        const res = await fetch(`https://so3h-backend.vercel.app/api/admin/student/edit/${id}`, {
+        const res = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/admin/student/edit/${id}`, {
         method: "PATCH",
         headers: {
             "Content-Type": "application/json",
